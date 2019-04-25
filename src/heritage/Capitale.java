5805efb99bc7b57c0b9e92a6f2add0b429de20d4
fep <franck.en.planck@gmail.com>
2019-04-25 11:28:05 +0200

    25/04/19 11:28 commit PUIS push

--- src/heritage/Capitale.java
+++ src/heritage/Capitale.java
@@ -5,6 +5,7 @@ public class Capitale extends Ville {
 		//  25/04/19 11:09 1er commit & push
 		//  25/04/19 11:12 commit & push
 		//  25/04/19 11:26 commit & push
+	//  25/04/19 11:28 commit PUIS push
 		
 		private String monument;
 		
