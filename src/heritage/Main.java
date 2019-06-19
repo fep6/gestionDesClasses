ed9d83533293c1ad3676268122d3f0141fbedbfa
fep <franck.en.planck@gmail.com>
2019-06-19 16:59:28 +0200

    Révision héritage

--- src/heritage/Main.java
+++ src/heritage/Main.java
@@ -13,43 +13,43 @@ class Main {
 		  
 	Ville v = new Ville();
 	System.out.println("Le nombre d'instances de la classe Ville est : " + Ville.nbreInstances);
-	System.out.println("Le nombre d'instances de la classe Ville est : " + Ville.getNombreInstancesBis());
+//	System.out.println("Le nombre d'instances bis de la classe Ville est : " + Ville.getNombreInstancesBis());
 	Ville v1 = new Ville("Marseille", 123456, "France");
 	System.out.println("Le nombre d'instances de la classe Ville est : " + Ville.nbreInstances);
-	System.out.println("Le nombre d'instances de la classe Ville est : " + Ville.getNombreInstancesBis());       
+	//	System.out.println("Le nombre d'instances bis de la classe Ville est : " + Ville.getNombreInstancesBis());       
 	Ville v2 = new Ville("Rio", 321654, "Brésil");
 	System.out.println("Le nombre d'instances de la classe Ville est : " + Ville.nbreInstances);
-	System.out.println("Le nombre d'instances de la classe Ville est : " + Ville.getNombreInstancesBis());
+	//	System.out.println("Le nombre d'instances bis de la classe Ville est : " + Ville.getNombreInstancesBis());
 
-//	System.out.println("\n v = "+v.getNom()+" ville de  "+v.getNombreHabitants()+ 
-//			" habitants se situant en "+v.getNomPays());
-//	System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ 
-//			" habitants se situant en "+v1.getNomPays());
-//	System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ 
-//			" habitants se situant en "+v2.getNomPays()+"\n\n");
+	System.out.println("\n v = "+v.getNom()+" ville de  "+v.getNombreHabitants()+ 
+			" habitants se situant en "+v.getNomPays());
+	System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ 
+			" habitants se situant en "+v1.getNomPays());
+	System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ 
+			" habitants se situant en "+v2.getNomPays()+"\n\n");
 //	        
 	/*
 	  Nous allons interchanger les Villes v1 et v2
 	  tout ça par l'intermédiaire d'un autre objet Ville.        
 	*/       
-//	Ville temp = new Ville();
-//	temp = v1;
-//	v1 = v2;
-//	v2 = temp;
-//	       
-//	System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
-//	System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+	Ville temp = new Ville();
+	temp = v1;
+	v1 = v2;
+	v2 = temp;
+	       
+	System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+	System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
 
 	/*       
 	  Nous allons maintenant interchanger leurs noms
 	  cette fois par le biais de leurs mutateurs.
 	*/   
-//	v1.setNom("Hong Kong");
-//	v2.setNom("Djibouti");
-//	      
-//	System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
-//	System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
-//	  
+	v1.setNom("Hong Kong");
+	v2.setNom("Djibouti");
+	      
+	System.out.println(" v1 = "+v1.getNom()+" ville de  "+v1.getNombreHabitants()+ " habitants se situant en "+v1.getNomPays());
+	System.out.println(" v2 = "+v2.getNom()+" ville de  "+v2.getNombreHabitants()+ " habitants se situant en "+v2.getNomPays()+"\n\n");
+	  
 //	  // Essai pour héritage
 //	Capitale cap = new Capitale();
 //	System.out.println("D'une capitale par défaut, cap.decrisToi()" + cap.decrisToi());
---
+++ src/heritage/MainHeritage.java
@@ -0,0 +1,11 @@
+package heritage;
+
+public class MainHeritage {
+
+	public static void main(String[] args) {
+		Capitale cap = new Capitale();
+		System.out.println(cap.decrisToi());
+
+	}
+
+}
