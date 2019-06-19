3c841188dca4895a2eb8d8461ad0e505d62ad225
fep <franck.en.planck@gmail.com>
2019-06-19 17:13:07 +0200

    https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/21424-gerez-les-heritages#/id/r-2181162

--- src/heritage/Capitale.java
+++ src/heritage/Capitale.java
@@ -8,52 +8,52 @@ public class Capitale extends Ville {
 		//  25/04/19 11:28 commit PUIS push
 		//  02/05/19 15:15 Changement nom de projet heritage -> gestionDesClasses 
 		
-		private String monument;
-		
-		//Constructeur par défaut
-		public Capitale() {
-			super();
-			monument = "aucun";
-		}
-		
-		//Constructeur d'initialisation de Capitale
-		public Capitale(String nom, int hab, String pays, String monument) {
-			super(nom, hab, pays);
-			this.monument = monument;
-		}
-		
-		
-		
-		
-		
-		
-		
-		// Description d'une capitale
-		public String decrisToi() {
-			System.out.println("Invocation de super.decrisToi");
-			String str = super.decrisToi() + "\n \t ==>>" + this.monument + " en est un monument";
-			return str; 
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		// @return le nom du monument
-		public  String getMonument() {
-			return monument;
-		}
-		
-		//Définit le nom du monument
-		public void setMonument(String monument) {
-			this.monument = monument;
-		}
+//		private String monument;
+//		
+//		//Constructeur par défaut
+//		public Capitale() {
+//			super();
+//			monument = "aucun";
+//		}
+//		
+//		//Constructeur d'initialisation de Capitale
+//		public Capitale(String nom, int hab, String pays, String monument) {
+//			super(nom, hab, pays);
+//			this.monument = monument;
+//		}
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		// Description d'une capitale
+//		public String decrisToi() {
+//			System.out.println("Invocation de super.decrisToi");
+//			String str = super.decrisToi() + "\n \t ==>>" + this.monument + " en est un monument";
+//			return str; 
+//		}
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		// @return le nom du monument
+//		public  String getMonument() {
+//			return monument;
+//		}
+//		
+//		//Définit le nom du monument
+//		public void setMonument(String monument) {
+//			this.monument = monument;
+//		}
 		
 
 }
